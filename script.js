fbf655e1c87e5c40b6b3102166709c6a41191dfa
Deluxe-Therapy <Houseofholistichealth@outlook.com>
2025-02-07 23:28:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -36,9 +36,9 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active1", "");
+    dots[i].className = dots[i].className.replace("active1", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active1";
+  dots[slideIndex-1].className += "active1";
 }
  
